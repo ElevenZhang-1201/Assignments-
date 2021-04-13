67512023f0d2c9e48c333c834b0ffe652adcde39
Eleven Zhang <elevenzhang@Elevens-MacBook.local>
2021-04-13 15:19:27 +0800

    lightyear

---
+++ week2.2.R
@@ -0,0 +1,10 @@
+c <- 300000
+lightMinute <- c*60
+lightHour <- lightMinute*60
+lightday <- lightHour *24
+lightyear <- lightday *365
+cat("length of light year:", lightyear, "\n", sep = "")
+cat("That's too long")
+
+
+# \n for new line following
